b912deafa3705a050759c5e4e49c49f3d0e0e4a1
mbright90 <michael.bright90@icloud.com>
2022-06-03 10:06:03 +0100

    Update javascript.js
    Adding further functions to enable sustained gameplay of rock, paper,
    scissors. Adding checks on userPlay function to ensure user has
    chosen a given option.

--- javascript.js
+++ javascript.js
@@ -3,14 +3,58 @@ const rockPaperScissorChoices = ['rock', 'paper', 'scissors'];
 
 function computerPlay() {
     let compChoiceIndex = Math.floor(Math.random()*3);
-    let compChoice = rockPaperScissorChoices[compChoiceIndex];
-    return compChoice;
+    let comp = rockPaperScissorChoices[compChoiceIndex];
+    return comp;
 }
 
 function userPlay() {
-    let userChoice = prompt('Please choose rock, paper or scissors: ').toLowerCase();
-    return userChoice;
+    let choosingTurn = true;
+    while (choosingTurn) {
+        var user = prompt('Please choose rock, paper or scissors: ').toLowerCase();
+        if (user === 'rock' || user ==='paper' || user === 'scissors') {
+            choosingTurn = false;
+        }
+    }
+    return user;
 }
 
-console.log(computerPlay())
-userPlay()
+function checkDraw(user, comp) {
+    if (user === comp) return true;
+}
+
+function checkResult(user, comp) {
+    if (user === 'rock' && comp === 'scissors' || user === 'paper' && comp === 'rock' || user === 'scissors' && comp === 'paper') {
+        return true;
+    } else {
+        return false
+    }
+}
+
+function playRound() {
+    // Initializing game values
+    let userChoice = userPlay();
+    let compChoice= computerPlay();
+
+    // Checking result of game
+    if (checkDraw(userChoice, compChoice)) {
+        alert(`Draw game. You chose ${userChoice} and computer chose ${compChoice}`)
+    } else if (checkResult(userChoice, compChoice)) {
+        alert(`You win! You chose ${userChoice} and the computer chose ${compChoice}`);
+        return true;
+    } else {
+        alert(`You lose! You chose ${userChoice} and the computer chose ${compChoice}`)
+    }
+}
+
+function game() {
+    let userScore = 0;
+    for (let i = 0; i < 5; i++) {
+        if (playRound()) {
+            ++userScore;
+        };
+        console.log(`You have won ${userScore} games`)
+    }
+    alert(`After five games, you have won ${userScore}!`)
+}
+
+game()
